f1a0685546bc1873e6806cda4261bc71a3998e93
rob tillaart <rob.tillaart@gmail.com>
2022-12-30 13:42:09 +0100

    add getType()

--- TCA9555.h
+++ TCA9555.h
@@ -2,7 +2,7 @@
 //
 //    FILE: TCA9555.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.4
+// VERSION: 0.1.5
 // PURPOSE: Arduino library for I2C TCA9555 16 channel port expander
 //    DATE: 2021-06-09
 //     URL: https://github.com/RobTillaart/TCA9555
@@ -12,7 +12,7 @@
 #include "Wire.h"
 
 
-#define TCA9555_LIB_VERSION               (F("0.1.4"))
+#define TCA9555_LIB_VERSION               (F("0.1.5"))
 
 #define TCA9555_OK                        0x00
 #define TCA9555_PIN_ERROR                 0x81
@@ -102,6 +102,7 @@ protected:
   uint8_t  _address;
   TwoWire* _wire;
   uint8_t  _error;
+  uint8_t  _type;
 };
 
 
